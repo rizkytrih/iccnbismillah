2bdbf137b5db1748dd6f054737a1c8f26eda6671
jetreel <jetreellider@yahoo.co.id>
2023-05-17 10:30:27 +0700

    add calender(salman)

--- index.php
+++ index.php
@@ -421,6 +421,34 @@
 						</div>
 						<aside class="sidebar col-md-3 col-md-pull-9 col-bordered">
 							<!-- Widget :: Latest Posts -->
+							<div>
+						<?php
+include 'Calendar.php';
+$calendar = new Calendar('2023-05-12');
+$calendar->add_event('Birthday', '2023-05-03', 1, 'green');
+$calendar->add_event('Doctors', '2023-05-04', 1, 'red');
+$calendar->add_event('Holiday', '2023-05-16', 7);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Event Calendar</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="calendar.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+	    <nav class="navtop">
+	    	<div>
+	    		<h1>Event Calendar</h1>
+	    	</div>
+	    </nav>
+		<div class="content home">
+			<?=$calendar?>
+		</div>
+	</body>
+</html>
+						</div>
 							<!-- /Widget :: Latest Posts -->
 
 
@@ -594,6 +622,8 @@
 								<!-- /Widget :: Flickr -->
 							</div>
 						</div>
+						
+
 					</div>
 				</div>
 				<div class="footer-copyright">
