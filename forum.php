07ae7f78989051620520efe4c8aa6f810b940359
Yoki <yokivalianda@gmail.com>
2023-05-19 14:18:46 +0700

    ubah tampilan berita.php dan forum.php dan navigasinya

---
+++ forum.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Forums | Responsive HTML5 Template</title>
+	<meta name="description" content="Stability - Responsive HTML5 Template - 1.8.1">
+	<meta name="author" content="http://themeforest.net/user/dan_fisher">
+	
+
+	<!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<!-- Google Web Fonts
+	================================================== -->
+	<link href='http://fonts.googleapis.com/css?family=Anton|Muli:300,400,400italic,300italic|Oswald' rel='stylesheet' type='text/css'>
+
+	<!-- CSS
+	================================================== -->
+	<!-- Base + Vendors CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fonts/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="vendor/owl-carousel/owl.carousel.css" media="screen">
+	<link rel="stylesheet" href="vendor/owl-carousel/owl.theme.css" media="screen">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" media="screen">
+	<link rel="stylesheet" href="vendor/mediaelement/mediaelementplayer.css" />
+
+	<!-- Theme CSS-->
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+
+	<!-- Skin CSS -->
+	<link rel="stylesheet" href="css/skins/red.css">
+
+	<!-- Custom CSS-->
+	<link rel="stylesheet" href="css/custom.css">
+
+	<!-- Head Libs -->
+	<script src="vendor/modernizr.js"></script>
+
+	<!--[if lt IE 9]>
+		
+		<script src="vendor/respond.min.js"></script>
+	<![endif]-->
+
+	<!--[if IE]>
+		<link rel="stylesheet" href="css/ie.css">
+	<![endif]-->
+	
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
+	
+</head>
+<body>
+
+	<div class="site-wrapper">
+		
+		<!-- Header -->
+		<?php include 'header/header.php';?>
+		<!-- Header / End -->
+
+		<!-- Main -->
+		<div class="main" role="main">
+
+			<!-- Page Heading -->
+			<section class="page-heading">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<h1>Forums</h1>
+						</div>
+						<div class="col-md-6">
+							<ul class="breadcrumb">
+								<li><a href="index.html">Home</a></li>
+								<li class="active">Forums</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Page Heading / End -->
+
+			<!-- Page Content -->
+			<section class="page-content">
+				<div class="container">
+
+					<div class="row">
+						<div class="content col-md-8">
+							<form action="#" class="search-form bbp-search-form">
+								<div class="input-group">
+									<input type="search" class="form-control" placeholder="Search here...">
+									<span class="input-group-btn">
+										<button class="btn" type="button"><i class="fa fa-search"></i></button>
+									</span>
+								</div>
+							</form>
+
+
+							<!-- Forum -->
+							<ul class="bbp-forums">
+								<li class="bbp-header">
+									<ul class="forum-titles">
+										<li class="bbp-forum-info">Forum</li>
+										<li class="bbp-forum-topic-count">Topics</li>
+										<li class="bbp-forum-reply-count">Posts</li>
+										<li class="bbp-forum-freshness">Freshness</li>
+									</ul>
+								</li><!-- .bbp-header -->
+
+								<li class="bbp-body">
+									<ul class="forum">
+										<li class="bbp-forum-info">
+											<a class="bbp-forum-title" href="#" title="General">General</a>
+											<div class="bbp-forum-content">Class aptent taciti</div>
+										</li>
+										<li class="bbp-forum-topic-count">2</li>
+										<li class="bbp-forum-reply-count">3</li>
+										<li class="bbp-forum-freshness">
+											<a href="#">1 month, 3 weeks ago</a>
+											<p class="bbp-topic-meta">
+												<span class="bbp-topic-freshness-author">
+													<a href="#" class="bbp-author-name">Dan Fisher</a>
+												</span>
+											</p>
+										</li>
+									</ul>
+
+									<ul class="forum">
+										<li class="bbp-forum-info">
+											<a class="bbp-forum-title" href="#" title="General">Support</a>
+											<div class="bbp-forum-content">Donec lobortis tortor et</div>
+										</li>
+										<li class="bbp-forum-topic-count">5</li>
+										<li class="bbp-forum-reply-count">13</li>
+										<li class="bbp-forum-freshness">
+											<a href="#">2 months, 2 weeks ago</a>
+											<p class="bbp-topic-meta">
+												<span class="bbp-topic-freshness-author">
+													<a href="#" class="bbp-author-name">Dan Fisher</a>
+												</span>
+											</p>
+										</li>
+									</ul>
+
+									<ul class="forum">
+										<li class="bbp-forum-info">
+											<a class="bbp-forum-title" href="#" title="General">FAQ</a>
+											<div class="bbp-forum-content">Praesent sollicitudin ante</div>
+										</li>
+										<li class="bbp-forum-topic-count">2</li>
+										<li class="bbp-forum-reply-count">3</li>
+										<li class="bbp-forum-freshness">
+											<a href="#">2 months, 3 weeks ago</a>
+											<p class="bbp-topic-meta">
+												<span class="bbp-topic-freshness-author">
+													<a href="#" class="bbp-author-name">Dan Fisher</a>
+												</span>
+											</p>
+										</li>
+									</ul>
+
+									<ul class="forum">
+										<li class="bbp-forum-info">
+											<a class="bbp-forum-title" href="#" title="General">Off Topic</a>
+											<div class="bbp-forum-content">Sed sit amet aliquam</div>
+										</li>
+										<li class="bbp-forum-topic-count">6</li>
+										<li class="bbp-forum-reply-count">15</li>
+										<li class="bbp-forum-freshness">
+											<a href="#">3 months, 1 week ago</a>
+											<p class="bbp-topic-meta">
+												<span class="bbp-topic-freshness-author">
+													<a href="#" class="bbp-author-name">Dan Fisher</a>
+												</span>
+											</p>
+										</li>
+									</ul>
+
+									<ul class="forum">
+										<li class="bbp-forum-info">
+											<a class="bbp-forum-title" href="#" title="General">Misc</a>
+											<div class="bbp-forum-content">Sed molestie dolor vitae</div>
+										</li>
+										<li class="bbp-forum-topic-count">12</li>
+										<li class="bbp-forum-reply-count">33</li>
+										<li class="bbp-forum-freshness">
+											<a href="#" title="Reply To: Hello World!">1 year ago</a>
+											<p class="bbp-topic-meta">
+												<span class="bbp-topic-freshness-author">
+													<a href="#" class="bbp-author-name">Dan Fisher</a>
+												</span>
+											</p>
+										</li>
+									</ul>
+								</li><!-- .bbp-body -->
+							</ul><!-- .forums-directory -->
+							<!-- Forum / End -->
+
+						</div>
+
+						<aside class="sidebar col-md-3 col-md-offset-1 col-bordered">
+							<hr class="visible-sm visible-xs lg">
+
+							<!-- Widget :: Categories -->
+							<div class="widget_categories widget widget__sidebar">
+								<h3 class="widget-title">Categories</h3>
+								<div class="widget-content">
+									<ul>
+										<li><a href="#">Web Design</a> (3)</li>
+										<li><a href="#">Illustration</a> (10)</li>
+										<li><a href="#">Logo Design</a> (12)</li>
+										<li><a href="#">Branding &amp; Identity</a> (8)</li>
+										<li><a href="#">WordPress</a> (3)</li>
+										<li><a href="#">HTML5 &amp; CSS3</a> (5)</li>
+										<li><a href="#">PHP/MySQl</a> (3)</li>
+									</ul>
+								</div>
+							</div>
+							<!-- /Widget :: Categories -->
+
+							<!-- Widget :: Tags Cloud -->
+							<div class="widget_tag_cloud widget widget__sidebar">
+								<h3 class="widget-title">Tags</h3>
+								<div class="widget-content">
+									<div class="tagcloud">
+										<a href="#">wordpress</a>
+										<a href="#">development</a>
+										<a href="#">php</a>
+										<a href="#">css3</a>
+										<a href="#">html5</a>
+										<a href="#">bootstrap</a>
+										<a href="#">programming</a>
+										<a href="#">web design</a>
+									</div>
+								</div>
+							</div>
+							<!-- /Widget :: Tags Cloud -->
+
+							<!-- Widget :: Latest Posts -->
+							<div class="latest-posts-widget widget widget__sidebar">
+								<h3 class="widget-title">Latest Posts</h3>
+								<div class="widget-content">
+									<ul class="latest-posts-list">
+										<li>
+											<figure class="thumbnail"><a href="#"><img src="http://placehold.it/60x60"></a></figure>
+											<span class="date">24/07/2013</span>
+											<h5 class="title"><a href="#">Duis placerat rhoncus arcu, sit amet aliquam leo</a></h5>
+										</li>
+										<li>
+											<figure class="thumbnail"><a href="#"><img src="http://placehold.it/60x60"></a></figure>
+											<span class="date">16/07/2013</span>
+											<h5 class="title"><a href="#">Mauris in arcu aliq, elementum nibh nec</a></h5>
+										</li>
+										<li>
+											<figure class="thumbnail"><a href="#"><img src="http://placehold.it/60x60"></a></figure>
+											<span class="date">14/07/2013</span>
+											<h5 class="title"><a href="#">Vestibulum in ligula rutrum faucibus interdum</a></h5>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<!-- /Widget :: Latest Posts -->
+						</aside>
+					</div>
+
+				</div>
+			</section>
+			<!-- Page Content / End -->
+            <?php include 'footer/footer.php';?>		
+		</div>
+		<!-- Main / End -->
+	</div>
+	
+	
+	
+	
+	
+	<!-- Javascript Files
+	================================================== -->
+	<script src="vendor/jquery-1.11.0.min.js"></script>
+	<script src="vendor/jquery-migrate-1.2.1.min.js"></script>
+	<script src="vendor/bootstrap.min.js"></script>
+	<script src="vendor/headhesive.min.js"></script>
+	<script src="vendor/fhmm.js"></script>
+	<script src="vendor/jquery.flickrfeed.js"></script>
+	<script src="vendor/isotope/isotope.pkgd.min.js"></script>
+	<script src="vendor/isotope/jquery.imagesloaded.min.js"></script>
+	<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+	<script src="vendor/owl-carousel/owl.carousel.min.js"></script>
+	<script src="vendor/jquery.fitvids.js"></script>
+	<script src="vendor/jquery.appear.js"></script>
+	<script src="vendor/jquery.stellar.min.js"></script>
+	<script src="vendor/snap.svg-min.js"></script>
+	<script src="vendor/mediaelement/mediaelement-and-player.min.js"></script>
+	<script src="vendor/jquery.twitter.js"></script>
+
+	<script src="js/custom.js"></script>
+
+
+	
+</body>
+</html>
--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 									<figure class="alignnone project-img">
 										<img src="http://placehold.it/346x220" alt="gambar" />
 										<div class="overlay">
-											<a href="#" class="dlink"><i class="fa fa-link"></i></a>
+											<a href="berita.php" class="dlink"><i class="fa fa-link"></i></a>
 										</div>
 									</figure>
 									<div class="project-desc">
@@ -40,7 +40,7 @@
 											</a>
 											<span class="date">May 29, 2014</span>
 										</div>
-										<h4 class="title"><a href="#">ini adalah berita 1</a></h4>
+										<h4 class="title"><a href="berita.php">ini adalah berita 1</a></h4>
 									</div>
 								</div>
 						</div>
@@ -50,7 +50,7 @@
 									<figure class="alignnone project-img">
 										<img src="http://placehold.it/346x220" alt="" />
 										<div class="overlay">
-											<a href="#" class="dlink"><i class="fa fa-link"></i></a>
+											<a href="berita.php" class="dlink"><i class="fa fa-link"></i></a>
 										</div>
 									</figure>
 									<div class="project-desc">
@@ -60,7 +60,7 @@
 											</a>
 											<span class="date">May 29, 2014</span>
 										</div>
-										<h4 class="title"><a href="#">ini adalah berita 2</a></h4>
+										<h4 class="title"><a href="berita.php">ini adalah berita 2</a></h4>
 									</div>
 								</div>
 						   </div>
@@ -70,7 +70,7 @@
 									<figure class="alignnone project-img">
 										<img src="http://placehold.it/346x220" alt="" />
 										<div class="overlay">
-											<a href="#" class="dlink"><i class="fa fa-link"></i></a>
+											<a href="berita.php" class="dlink"><i class="fa fa-link"></i></a>
 										</div>
 									</figure>
 									<div class="project-desc">
@@ -80,7 +80,7 @@
 											</a>
 											<span class="date">May 29, 2014</span>
 										</div>
-										<h4 class="title"><a href="#">ini adalah berita 3</a></h4>
+										<h4 class="title"><a href="berita.php">ini adalah berita 3</a></h4>
 									</div>
 								</div>
 						   </div>
@@ -389,82 +389,8 @@ $calendar->add_event('Holiday', '2023-05-16', 7);
 			</div>
 			<!-- Social Links / End -->
 
-			<!-- Footer -->
-			<footer class="footer" id="footer">
-				<div class="footer-widgets">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-8 col-md-9">
-								<!-- Widget :: Text Widget -->
-								<div class="widget_text widget widget__footer">
-									<h3 class="widget-title">About Us</h3>
-									<div class="widget-content">
-										<p>Mauris et vulputate est. Integer viverra nunc vitae facilisis cursus. Quisque ac ultricies quam, a molestie leo. Nulla convallis turpis id vulputate aliquam. Nulla facilisi. Nam tincidunt odio dignissim dui lobortis adipiscing.</p>
-
-										<p>Nullam volutpat quis sem id iaculis. Fusce interdum, quam ac aliquam vulputate, orci nisl imperdiet turpis, non posuere massa ipsum et lorem. Pellentesque facilisis, orci nec gravida accumsan, massa elit hendrerit lacus, tortor.</p>
-
-										<a href="#" class="btn btn-primary">Learn More</a>
-									</div>
-								</div>
-								<!-- /Widget :: Text Widget -->
-							</div>
-							<div class="col-sm-4 col-md-3">
-								<!-- Widget :: Contacts Info -->
-								<div class="contacts-widget widget widget__footer">
-									<h3 class="widget-title">Contact Us</h3>
-									<div class="widget-content">
-										<ul class="contacts-info-list">
-											<li>
-												<i class="fa fa-map-marker"></i>
-												<div class="info-item">
-													Stability LTD., Old Town Avenue, New York, USA 23000
-												</div>
-											</li>
-											<li>
-												<i class="fa fa-phone"></i>
-												<div class="info-item">
-													+1700 124-5678<br>
-													+1700 124-5678
-												</div>
-											</li>
-											<li>
-												<i class="fa fa-envelope"></i>
-												<span class="info-item">
-													<a href="mailto:info@dan-fisher.com">info@dan-fisher.com</a>
-												</span>
-											</li>
-										</ul>
-									</div>
-								</div>
-								<!-- /Widget :: Contacts Info -->
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="footer-copyright">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-6 col-md-4">
-								Copyright &copy; 2023  <a href="index.html">ICCN</a> &nbsp;| &nbsp;All Rights Reserved
-							</div>
-							<div class="col-sm-6 col-md-8">
-								<div class="social-links-wrapper">
-									<span class="social-links-txt">Connect with us</span>
-									<ul class="social-links social-links__dark">
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-										<li><a href="#"><i class="fa fa-instagram"></i></a></li>
-										<li><a href="#"><i class="fa fa-rss"></i></a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</footer>
-			<!-- Footer / End -->
-
+			<?php include 'footer/footer.php';?>
+		
 		</div>
 		<!-- Main / End -->
 	</div>
