2b6ffb1a3f04a249565e3da6fd08644b2e76765c
alfa777777 <salmanalfarizi01999@gmail.com>
2023-05-27 08:12:33 +0700

    tambah map pada event.php

--- event.php
+++ event.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php include 'header/header.php';
-include 'header/koneksi.php';
-// kalau tidak ada id di query string
-if( !isset($_GET['id']) ){
-    header('Location: index.php');
-}
+	<?php include 'header/header.php';
+	include 'header/koneksi.php';
+	// kalau tidak ada id di query string
+	if (!isset($_GET['id'])) {
+		header('Location: index.php');
+	}
 
-//ambil id dari query string
-$id = $_GET['id'];
+	//ambil id dari query string
+	$id = $_GET['id'];
 
-// buat query untuk mengupdate jumlah view
-$updateQuery = mysqli_query($connection, "UPDATE acara SET dilihat = dilihat + 1 WHERE id='$id'");
-if ($updateQuery) {
-	// Jika berhasil diupdate, ambil kembali data berita setelah penambahan view
+	// buat query untuk mengupdate jumlah view
+	$updateQuery = mysqli_query($connection, "UPDATE acara SET dilihat = dilihat + 1 WHERE id='$id'");
+	if ($updateQuery) {
+		// Jika berhasil diupdate, ambil kembali data berita setelah penambahan view
+		$query = mysqli_query($connection, "SELECT * FROM acara JOIN admin ON admin.id_admin = acara.id_admin WHERE id='$id'");
+		$d = mysqli_fetch_assoc($query);
+
+	} else {
+		// Jika query gagal dijalankan, tampilkan pesan error
+		die("Gagal mengupdate jumlah view...");
+	}
+
+	// buat query untuk ambil data dari database
 	$query = mysqli_query($connection, "SELECT * FROM acara JOIN admin ON admin.id_admin = acara.id_admin WHERE id='$id'");
 	$d = mysqli_fetch_assoc($query);
 
-} else {
-  // Jika query gagal dijalankan, tampilkan pesan error
-  die("Gagal mengupdate jumlah view...");
-}
-
-// buat query untuk ambil data dari database
-$query= mysqli_query($connection, "SELECT * FROM acara JOIN admin ON admin.id_admin = acara.id_admin WHERE id='$id'");
-$d = mysqli_fetch_assoc($query);
-
-// jika data yang di-edit tidak ditemukan
-if( mysqli_num_rows($query) < 1 ){
-    die("data tidak ditemukan...");
-}
-?>
+	// jika data yang di-edit tidak ditemukan
+	if (mysqli_num_rows($query) < 1) {
+		die("data tidak ditemukan...");
+	}
+	?>
 
 
 
@@ -42,7 +43,7 @@ if( mysqli_num_rows($query) < 1 ){
 	<title>Post | Responsive HTML5 Template</title>
 	<meta name="description" content="Stability - Responsive HTML5 Template - 1.8.1">
 	<meta name="author" content="http://themeforest.net/user/dan_fisher">
-	
+
 
 	<!-- Mobile Specific Metas
 	================================================== -->
@@ -50,7 +51,8 @@ if( mysqli_num_rows($query) < 1 ){
 
 	<!-- Google Web Fonts
 	================================================== -->
-	<link href='http://fonts.googleapis.com/css?family=Anton|Muli:300,400,400italic,300italic|Oswald' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Anton|Muli:300,400,400italic,300italic|Oswald' rel='stylesheet'
+		type='text/css'>
 
 	<!-- CSS
 	================================================== -->
@@ -84,7 +86,7 @@ if( mysqli_num_rows($query) < 1 ){
 	<!--[if IE]>
 		<link rel="stylesheet" href="css/ie.css">
 	<![endif]-->
-	
+
 	<!-- Favicons
 	================================================== -->
 	<link rel="shortcut icon" href="images/favicon.ico">
@@ -93,18 +95,19 @@ if( mysqli_num_rows($query) < 1 ){
 	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
 	<link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-144x144.png">
 
-    <style>
-    .containers {
-      max-width: 1800px;
-      margin: 0 auto;
-      padding: 50px;
-      background-color: #f2f2f2;
-      border-radius: 5px;
-      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-      text-align: justify;
-    }
-  </style>
+	<style>
+		.containers {
+			max-width: 1800px;
+			margin: 0 auto;
+			padding: 50px;
+			background-color: #f2f2f2;
+			border-radius: 5px;
+			box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+			text-align: justify;
+		}
+	</style>
 </head>
+
 <body>
 
 	<div class="site-wrapper">
@@ -125,27 +128,70 @@ if( mysqli_num_rows($query) < 1 ){
 									<i class="fa fa-file"></i>
 								</div>
 								<header class="entry-header">
-									<h2><?php echo $d['nama_event']; ?></h2>
+									<h2>
+										<?php echo $d['nama_event']; ?>
+									</h2>
 									<div class="entry-meta">
-										<span class="entry-date"><?php echo $d['tgl_posting']; ?></span>
-										<span class="entry-comments"><a href="#"><?php echo $d['dilihat']; ?> Views</a></span>
+										<span class="entry-date">
+											<?php echo $d['tgl_posting']; ?>
+										</span>
+										<span class="entry-comments"><a href="#">
+												<?php echo $d['dilihat']; ?> Views
+											</a></span>
 										<span class="entry-category">in <a href="#">Event</a></span>
-										<span class="entry-author">by <a href="#"><?php echo $d['nama_lengkap']; ?></a></span>
+										<span class="entry-author">by <a href="#">
+												<?php echo $d['nama_lengkap']; ?>
+											</a></span>
 									</div>
 								</header>
 								<figure class="alignnone entry-thumb">
 									<img src="gambar/<?php echo $d['gambar_event'] ?>" alt="">
 								</figure>
 								<div class="containers">
-									
-                                    <br><?php echo $d['deskripsi_event']; ?>
-                                    <br>
-                                    <br>
-                                    Tanggal: <?php echo $d['tanggal']; ?>
-                                    <br>
-                                    Alamat: <?php echo $d['alamat']; ?>
-                    
+
+									<br>
+									<?php echo $d['deskripsi_event']; ?>
+									<br>
+									<br>
+									Tanggal:
+									<?php echo $d['tanggal']; ?>
+									<br>
+									Alamat:
+									<?php echo $d['alamat']; ?>
+
 								</div>
+								<div class="spacer-lg"></div>
+								<div id="peta" style="width: 100%;height: 80vh;">
+									<script src="https://api.mapbox.com/mapbox-gl-js/v2.5.0/mapbox-gl.js"></script>
+									<link href="https://api.mapbox.com/mapbox-gl-js/v2.5.0/mapbox-gl.css"
+										rel="stylesheet" />
+									<script
+										src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"></script>
+
+									<script>
+										mapboxgl.accessToken = 'pk.eyJ1Ijoic2FsbWFuNzc3NyIsImEiOiJjbGk1YTJ6Y3MxaXUxM2RvNGYwMmlpNGJyIn0.un_wQtTvZ7DcW-S6Wnnprw';
+										var map = new mapboxgl.Map({
+											container: 'peta',
+											style: 'mapbox://styles/mapbox/streets-v11',
+											center: [<?php echo $d['lokasi_longitude']; ?>, <?php echo $d['lokasi_latitude']; ?>],
+											zoom: 9
+										});
+
+										new mapboxgl.Marker().setLngLat([<?php echo $d['lokasi_longitude']; ?>, <?php echo $d['lokasi_latitude']; ?>])
+											.addTo(map)
+
+										var geocoder = new MapboxGeocoder({
+											accessToken: mapboxgl.accessToken,
+											mapboxgl: mapboxgl,
+											marker: false,
+											placeholder: 'Masukan kata kunci...',
+											zoom: 20
+										});
+
+										map.addControl(geocoder);
+									</script>
+								</div>
+
 							</article>
 							<!-- Post (Standard Format) / End -->
 
@@ -166,10 +212,13 @@ if( mysqli_num_rows($query) < 1 ){
 												</div>
 											</div>
 											<div class="comment-reply">
-												<a href="#" class="btn btn-sm btn-default"><i class="fa fa-reply"></i> Reply</a>
+												<a href="#" class="btn btn-sm btn-default"><i class="fa fa-reply"></i>
+													Reply</a>
 											</div>
 											<div class="comment-body">
-												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet venenatis orci, ut tempus ipsum. Donec sit amet massa et dui ultricies eleifend. Curabitur at nibh ante.
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit
+												amet venenatis orci, ut tempus ipsum. Donec sit amet massa et dui
+												ultricies eleifend. Curabitur at nibh ante.
 											</div>
 										</div>
 
@@ -186,10 +235,13 @@ if( mysqli_num_rows($query) < 1 ){
 														</div>
 													</div>
 													<div class="comment-reply">
-														<a href="#" class="btn btn-sm btn-default"><i class="fa fa-reply"></i> Reply</a>
+														<a href="#" class="btn btn-sm btn-default"><i
+																class="fa fa-reply"></i> Reply</a>
 													</div>
 													<div class="comment-body">
-														Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet venenatis orci, ut tempus ipsum. Donec sit amet massa et dui ultricies eleifend. 
+														Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+														sit amet venenatis orci, ut tempus ipsum. Donec sit amet massa
+														et dui ultricies eleifend.
 													</div>
 												</div>
 											</li>
@@ -205,10 +257,13 @@ if( mysqli_num_rows($query) < 1 ){
 														</div>
 													</div>
 													<div class="comment-reply">
-														<a href="#" class="btn btn-sm btn-default"><i class="fa fa-reply"></i> Reply</a>
+														<a href="#" class="btn btn-sm btn-default"><i
+																class="fa fa-reply"></i> Reply</a>
 													</div>
 													<div class="comment-body">
-														Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet venenatis orci, ut tempus ipsum. Donec sit amet massa et dui ultricies eleifend. 
+														Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+														sit amet venenatis orci, ut tempus ipsum. Donec sit amet massa
+														et dui ultricies eleifend.
 													</div>
 												</div>
 											</li>
@@ -226,10 +281,13 @@ if( mysqli_num_rows($query) < 1 ){
 												</div>
 											</div>
 											<div class="comment-reply">
-												<a href="#" class="btn btn-sm btn-default"><i class="fa fa-reply"></i> Reply</a>
+												<a href="#" class="btn btn-sm btn-default"><i class="fa fa-reply"></i>
+													Reply</a>
 											</div>
 											<div class="comment-body">
-												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet venenatis orci, ut tempus ipsum. Donec sit amet massa et dui ultricies eleifend. Curabitur at nibh ante.
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit
+												amet venenatis orci, ut tempus ipsum. Donec sit amet massa et dui
+												ultricies eleifend. Curabitur at nibh ante.
 											</div>
 										</div>
 									</li>
@@ -240,22 +298,23 @@ if( mysqli_num_rows($query) < 1 ){
 							<!-- Comments Form -->
 							<div id="respond" class="comment-respond">
 								<h3 class="reply-title">Leave a reply</h3>
-								<p class="comment-notes text-muted"><i>Your email address will not be published. Required fields are marked <span class="required">*</span></i></p>
+								<p class="comment-notes text-muted"><i>Your email address will not be published.
+										Required fields are marked <span class="required">*</span></i></p>
 
 								<form action="#" method="POST" role="form">
-								
+
 									<div class="row">
 										<div class="col-md-6">
 											<div class="form-group">
 												<label for="">Name <span class="required">*</span></label>
 												<input type="text" class="form-control" id="">
 											</div>
-											
+
 											<div class="form-group">
 												<label for="">Email <span class="required">*</span></label>
 												<input type="email" class="form-control" id="">
 											</div>
-											
+
 											<div class="form-group">
 												<label for="">Website</label>
 												<input type="url" class="form-control" id="">
@@ -267,16 +326,16 @@ if( mysqli_num_rows($query) < 1 ){
 										<label for="">Comment</label>
 										<textarea cols="30" rows="10" class="form-control"></textarea>
 									</div>
-								
+
 									<button type="submit" class="btn btn-primary">Send Comment</button>
 								</form>
 							</div>
 							<!-- Comments Form / End -->
-							
+
 						</div>
 
 						<aside class="sidebar col-md-3 col-md-offset-1 col-bordered">
-							
+
 							<hr class="visible-sm visible-xs lg">
 
 							<!-- Widget :: Categories -->
@@ -320,19 +379,28 @@ if( mysqli_num_rows($query) < 1 ){
 								<div class="widget-content">
 									<ul class="latest-posts-list">
 										<li>
-											<figure class="thumbnail"><a href="#"><img src="http://placehold.it/60x60"></a></figure>
+											<figure class="thumbnail"><a href="#"><img
+														src="http://placehold.it/60x60"></a>
+											</figure>
 											<span class="date">24/07/2013</span>
-											<h5 class="title"><a href="#">Duis placerat rhoncus arcu, sit amet aliquam leo</a></h5>
+											<h5 class="title"><a href="#">Duis placerat rhoncus arcu, sit amet aliquam
+													leo</a></h5>
 										</li>
 										<li>
-											<figure class="thumbnail"><a href="#"><img src="http://placehold.it/60x60"></a></figure>
+											<figure class="thumbnail"><a href="#"><img
+														src="http://placehold.it/60x60"></a>
+											</figure>
 											<span class="date">16/07/2013</span>
-											<h5 class="title"><a href="#">Mauris in arcu aliq, elementum nibh nec</a></h5>
+											<h5 class="title"><a href="#">Mauris in arcu aliq, elementum nibh nec</a>
+											</h5>
 										</li>
 										<li>
-											<figure class="thumbnail"><a href="#"><img src="http://placehold.it/60x60"></a></figure>
+											<figure class="thumbnail"><a href="#"><img
+														src="http://placehold.it/60x60"></a>
+											</figure>
 											<span class="date">14/07/2013</span>
-											<h5 class="title"><a href="#">Vestibulum in ligula rutrum faucibus interdum</a></h5>
+											<h5 class="title"><a href="#">Vestibulum in ligula rutrum faucibus
+													interdum</a></h5>
 										</li>
 									</ul>
 								</div>
@@ -345,16 +413,16 @@ if( mysqli_num_rows($query) < 1 ){
 			</section>
 			<!-- Page Content / End -->
 
-			<?php include 'footer/footer.php';?>
-			
+			<?php include 'footer/footer.php'; ?>
+
 		</div>
 		<!-- Main / End -->
 	</div>
-	
-	
-	
-	
-	
+
+
+
+
+
 	<!-- Javascript Files
 	================================================== -->
 	<script src="vendor/jquery-1.11.0.min.js"></script>
@@ -377,6 +445,7 @@ if( mysqli_num_rows($query) < 1 ){
 	<script src="js/custom.js"></script>
 
 
-	
+
 </body>
+
 </html>
